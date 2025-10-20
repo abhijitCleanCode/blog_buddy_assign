75d7f5e440170d04b25b114a4c3f380caf06315a
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2025-10-20 15:13:49 +0530

    fix: deployment v4

--- providers/trpc-provider.tsx
+++ providers/trpc-provider.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {trpc} from "@/lib/trpc/trpc-client";
+import { trpc } from "@/lib/trpc/trpc-client";
 import { httpBatchLink } from "@trpc/client";
 import { ReactNode, useState } from "react";
 
+function getBaseUrl() {
+    if (typeof window !== 'undefined') return ''; // browser should use relative url
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // Vercel production
+    if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL; // optional fallback
+    return 'http://localhost:3000'; // local dev
+}
+
 export function TRPCProvider({ children }: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient());
+
     const [trpcClient] = useState(() =>
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: "/api/trpc",
+                    url: `${getBaseUrl()}/api/trpc`,
                 }),
             ],
         })
